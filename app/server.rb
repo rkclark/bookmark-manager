52c0f6f75fd447b46a12560352a47d15cf8d97f3
Rick Clark <rkingsleyclark@gmail.com>
2017-01-27 11:22:21 +0000

    Added partials to layout.erb

--- app/server.rb
+++ app/server.rb
@@ -1,9 +1,12 @@
 class BookmarkManager < Sinatra::Base
-  
+
   enable :sessions
   enable :method_override
   set :session_secret, 'super secret'
   register Sinatra::Flash
+  register Sinatra::Partial
+  set :partial_template_engine, :erb
+  enable :partial_underscores
   use Rack::MethodOverride
 
   get '/' do
